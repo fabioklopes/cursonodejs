04d64b5c2349869d01b5c97fdbe22f90bf69eadb
Fábio Klevinskas Lopes <fabioklopes@live.com>
2026-03-18 18:54:58 -0300

    novos ajustes para rotas com arquivos HTML

--- app.js
+++ app.js
@@ -4,32 +4,31 @@ const app = express();
 
 // rota principal
 app.get('/', function(req, res) {
-    res.send('Olá Mundo!')
+    res.sendFile(__dirname + '/html/index.html');
 });
 
 
 // cadastro de alunos
 app.get('/aluno', function(req, res) {
-    res.send('Rota de cadastro de alunos');
+    res.sendFile(__dirname + '/html/aluno.html');
 });
 
 
 // solicitação de presença
 app.get('/presenca', function(req, res) {
-    res.send('Rota de solicitação de presença');
+    res.sendFile(__dirname + '/html/presenca.html');
 });
 
 
 // login
 app.get('/login', function(req, res) {
-    res.send('Rota de login');
+    res.sendFile(__dirname + '/html/login.html');
 });
 
 
 // execução da aplicação
 app.listen(8080, function() {
     console.clear();
-    console.log('-----------------------');
     console.log('Servidor funcionando...');
-    console.log('Acesse http://localhost:8080 para ver o app');
+    console.log('Acesse http://localhost:8080 para ver o app.');
 });
