aa978a88d8db3ff36247effde76692c7b5fc6489
Fábio Klevinskas Lopes <fabioklopes@live.com>
2026-03-19 08:29:06 -0300

    adaptações para uso do handlebars e template

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
 const express = require('express');
+const path = require('path');
+const { engine } = require('express-handlebars');
 const app = express();
 
 
+// engine de template
+app.engine('handlebars', engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views', 'layouts')
+}));
+
+
 // rota principal
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/html/index.html');
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Gerenciador de Presenças',
+    });
 });
 
 
 // cadastro de alunos
 app.get('/aluno', function(req, res) {
-    res.sendFile(__dirname + '/html/aluno.html');
+    res.sendFile(path.join(__dirname, 'views', 'aluno.html'));
 });
 
 
 // solicitação de presença
 app.get('/presenca', function(req, res) {
-    res.sendFile(__dirname + '/html/presenca.html');
+    res.sendFile(path.join(__dirname, 'views', 'presenca.html'));
 });
 
 
 // login
 app.get('/login', function(req, res) {
-    res.sendFile(__dirname + '/html/login.html');
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
 
 
-// execução da aplicação
+// configurações de template execução da aplicação
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
 app.listen(8080, function() {
     console.clear();
     console.log('Servidor funcionando...');
